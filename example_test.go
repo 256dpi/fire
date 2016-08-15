c98d618aaa21245836140be98662eb8cea682de8
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-08-15 15:36:28 +0200

    added example

---
+++ example_test.go
@@ -0,0 +1,94 @@
+package fire
+
+import (
+	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2"
+	"github.com/gin-gonic/gin"
+	"fmt"
+)
+
+type Application struct {
+	Base      `bson:",inline" fire:"application:applications"`
+	Name      string   `json:"name" valid:"required"`
+	Key       string   `json:"key" valid:"required" fire:"identifiable"`
+	Secret    []byte   `json:"secret" valid:"required" fire:"verifiable"`
+	Scopes    []string `json:"scopes" valid:"required" fire:"grantable"`
+	Callbacks []string `json:"callbacks" valid:"required" fire:"callable"`
+}
+
+type User struct {
+	Base     `bson:",inline" fire:"user:users"`
+	FullName string `json:"full_name" valid:"required"`
+	Email    string `json:"email" valid:"required" fire:"identifiable"`
+	Password []byte `json:"-" valid:"required" fire:"verifiable"`
+}
+
+type Post struct {
+	Base     `bson:",inline" fire:"post:posts"`
+	Title    string  `json:"title" valid:"required" bson:"title" fire:"filterable,sortable"`
+	TextBody string  `json:"text-body" valid:"-" bson:"text_body"`
+	Comments HasMany `json:"-" valid:"-" bson:"-" fire:"comments:comments"`
+}
+
+type Comment struct {
+	Base    `bson:",inline" fire:"comment:comments"`
+	Message string         `json:"message" valid:"required"`
+	Parent  *bson.ObjectId `json:"-" valid:"-" fire:"parent:comments"`
+	PostID  bson.ObjectId  `json:"-" valid:"required" bson:"post_id" fire:"post:posts"`
+}
+
+func Example() {
+	// connect to database
+	sess, err := mgo.Dial("mongodb://0.0.0.0:27017/fire")
+	if err != nil {
+		panic(err)
+	}
+
+	// defer close
+	defer sess.Close()
+
+	// get db
+	db := sess.DB("")
+
+	// create authenticator
+	authenticator := NewAuthenticator(db,
+		&User{},
+		&Application{},
+		"a-very-long-secret",
+	)
+
+	// create endpoint
+	endpoint := NewEndpoint(db)
+
+	// add post
+	endpoint.AddResource(&Resource{
+		Model: &Post{},
+		Authorizer: authenticator.Authorizer("admin"),
+	})
+
+	// add comment
+	endpoint.AddResource(&Resource{
+		Model: &Comment{},
+		Authorizer: authenticator.Authorizer("admin"),
+	})
+
+	// create router
+	router := gin.New()
+
+	// register authenticator
+	authenticator.Register("auth", router)
+
+	// register api
+	endpoint.Register("api", router)
+
+	fmt.Println("server ready to run")
+
+	// run server
+	//err = router.Run("localhost:8080")
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	// Output:
+	// server ready to run
+}
--- meta_test.go
+++ meta_test.go
@@ -7,36 +7,6 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-type Application struct {
-	Base      `bson:",inline" fire:"application:applications"`
-	Name      string   `json:"name" valid:"required"`
-	Key       string   `json:"key" valid:"required" fire:"identifiable"`
-	Secret    []byte   `json:"secret" valid:"required" fire:"verifiable"`
-	Scopes    []string `json:"scopes" valid:"required" fire:"grantable"`
-	Callbacks []string `json:"callbacks" valid:"required" fire:"callable"`
-}
-
-type User struct {
-	Base     `bson:",inline" fire:"user:users"`
-	FullName string `json:"full_name" valid:"required"`
-	Email    string `json:"email" valid:"required" fire:"identifiable"`
-	Password []byte `json:"-" valid:"required" fire:"verifiable"`
-}
-
-type Post struct {
-	Base     `bson:",inline" fire:"post:posts"`
-	Title    string  `json:"title" valid:"required" bson:"title" fire:"filterable,sortable"`
-	TextBody string  `json:"text-body" valid:"-" bson:"text_body"`
-	Comments HasMany `json:"-" valid:"-" bson:"-" fire:"comments:comments"`
-}
-
-type Comment struct {
-	Base    `bson:",inline" fire:"comment:comments"`
-	Message string         `json:"message" valid:"required"`
-	Parent  *bson.ObjectId `json:"-" valid:"-" fire:"parent:comments"`
-	PostID  bson.ObjectId  `json:"-" valid:"required" bson:"post_id" fire:"post:posts"`
-}
-
 type malformedBase struct {
 	Base
 }
