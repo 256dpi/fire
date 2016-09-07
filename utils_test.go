7234d59ee1eb0e8d13d1676dd6713ce11322f078
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-09-07 18:27:12 +0200

    remove that boring example

--- utils_test.go
+++ utils_test.go
@@ -7,7 +7,30 @@ import (
 	"github.com/labstack/echo/engine"
 	"github.com/labstack/echo/test"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+type Post struct {
+	Base       `bson:",inline" fire:"posts"`
+	Title      string  `json:"title" valid:"required" bson:"title" fire:"filterable,sortable"`
+	Published  bool    `json:"published" valid:"-" fire:"filterable"`
+	TextBody   string  `json:"text-body" valid:"-" bson:"text_body"`
+	Comments   HasMany `json:"-" valid:"-" bson:"-" fire:"comments:comments:post"`
+	Selections HasMany `json:"-" valid:"-" bson:"-" fire:"selections:selections:posts"`
+}
+
+type Comment struct {
+	Base    `bson:",inline" fire:"comments"`
+	Message string         `json:"message" valid:"required"`
+	Parent  *bson.ObjectId `json:"-" valid:"-" fire:"parent:comments"`
+	PostID  bson.ObjectId  `json:"-" valid:"required" bson:"post_id" fire:"post:posts"`
+}
+
+type Selection struct {
+	Base    `bson:",inline" fire:"selections:selections"`
+	Name    string          `json:"name" valid:"required"`
+	PostIDs []bson.ObjectId `json:"-" valid:"-" bson:"post_ids" fire:"posts:posts"`
+}
 
 var session *mgo.Session
 
