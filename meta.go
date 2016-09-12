31aaca2eb6ea2a9379df05db0ee471823b14c463
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-09-12 17:31:33 +0200

    combined files

--- meta.go
+++ meta.go
@@ -230,3 +230,37 @@ func (m *Meta) MakeSlice() interface{} {
 	pointer.Elem().Set(slice)
 	return pointer.Interface()
 }
+
+func getJSONFieldName(field *reflect.StructField) string {
+	tag := field.Tag.Get("json")
+	values := strings.Split(tag, ",")
+
+	// check for "-"
+	if tag == "-" {
+		return ""
+	}
+
+	// check first value
+	if len(tag) > 0 || len(values[0]) > 0 {
+		return values[0]
+	}
+
+	return field.Name
+}
+
+func getBSONFieldName(field *reflect.StructField) string {
+	tag := field.Tag.Get("bson")
+	values := strings.Split(tag, ",")
+
+	// check for "-"
+	if tag == "-" {
+		return ""
+	}
+
+	// check first value
+	if len(tag) > 0 || len(values[0]) > 0 {
+		return values[0]
+	}
+
+	return strings.ToLower(field.Name)
+}
