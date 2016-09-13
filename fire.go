77fa3ccca0fa54ffe713a3eef1058615b009c17d
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-09-13 12:59:11 +0200

    cleanup

--- fire.go
+++ fire.go
@@ -1,3 +1,5 @@
+// Package fire implements a small and opinionated framework for Go providing
+// Ember compatible JSON APIs.
 package fire
 
 // Map is a general purpose map used for configuration.
--- utils_test.go
+++ utils_test.go
@@ -6,34 +6,10 @@ import (
 	"net"
 	"time"
 
-	"github.com/gonfire/fire/model"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine"
 	"github.com/labstack/echo/engine/standard"
-	"gopkg.in/mgo.v2/bson"
 )
-
-type Post struct {
-	model.Base `json:"-" bson:",inline" fire:"posts"`
-	Title      string        `json:"title" valid:"required" bson:"title" fire:"filterable,sortable"`
-	Published  bool          `json:"published" valid:"-" fire:"filterable"`
-	TextBody   string        `json:"text-body" valid:"-" bson:"text_body"`
-	Comments   model.HasMany `json:"-" valid:"-" bson:"-" fire:"comments:comments:post"`
-	Selections model.HasMany `json:"-" valid:"-" bson:"-" fire:"selections:selections:posts"`
-}
-
-type Comment struct {
-	model.Base `json:"-" bson:",inline" fire:"comments"`
-	Message    string         `json:"message" valid:"required"`
-	Parent     *bson.ObjectId `json:"-" valid:"-" fire:"parent:comments"`
-	PostID     bson.ObjectId  `json:"-" valid:"required" bson:"post_id" fire:"post:posts"`
-}
-
-type Selection struct {
-	model.Base `json:"-" bson:",inline" fire:"selections:selections"`
-	Name       string          `json:"name" valid:"required"`
-	PostIDs    []bson.ObjectId `json:"-" valid:"-" bson:"post_ids" fire:"posts:posts"`
-}
 
 type testComponent struct{}
 
