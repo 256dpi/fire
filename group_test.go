eb8d27664031b4f779c7d978a281d3ac11479619
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-10-26 10:04:18 +0300

    added missing test

--- group_test.go
+++ group_test.go
@@ -1,6 +1,8 @@
 package fire
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,4 +17,12 @@ func TestGroup(t *testing.T) {
 	group.Add(controller)
 	assert.Equal(t, []*Controller{controller}, group.List())
 	assert.Equal(t, controller, group.Find("posts"))
+}
+
+func TestGroupEndpointMissingController(t *testing.T) {
+	group := NewGroup()
+
+	testRequest(group.Endpoint("api"), "GET", "api/foo", nil, "", func(r *httptest.ResponseRecorder, rq *http.Request) {
+		assert.Equal(t, http.StatusNotFound, r.Result().StatusCode)
+	})
 }
