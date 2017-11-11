80cdcfc35c973735c3c8a4fe12d8aa1a29a05900
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-11-11 21:27:33 +0100

    added stack abort test

--- group_test.go
+++ group_test.go
@@ -1,10 +1,12 @@
 package fire
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/256dpi/stack"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -25,4 +27,36 @@ func TestGroupEndpointMissingController(t *testing.T) {
 	testRequest(group.Endpoint("api"), "GET", "api/foo", nil, "", func(r *httptest.ResponseRecorder, rq *http.Request) {
 		assert.Equal(t, http.StatusNotFound, r.Result().StatusCode)
 	})
+}
+
+func TestGroupStackAbort(t *testing.T) {
+	wait := make(chan struct{})
+
+	group := NewGroup()
+	group.Reporter = func(err error) {
+		assert.Equal(t, "foo", err.Error())
+		close(wait)
+	}
+	group.Add(&Controller{
+		Model: &Post{},
+		Store: testStore,
+		Authorizers: []Callback{
+			func(*Context) error {
+				stack.Abort(errors.New("foo"))
+				return nil
+			},
+		},
+	})
+
+	testRequest(group.Endpoint(""), "GET", "posts", nil, "", func(r *httptest.ResponseRecorder, rq *http.Request) {
+		assert.Equal(t, http.StatusInternalServerError, r.Result().StatusCode)
+		assert.JSONEq(t, `{
+			"errors": [{
+				"status": "500",
+				"title": "Internal Server Error"
+			}]
+		}`, r.Body.String())
+	})
+
+	<-wait
 }
