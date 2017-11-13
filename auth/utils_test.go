4d6285368224e7bcee7357c82e6fb7d2f085d000
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-11-14 00:13:53 +0100

    removed unused things

--- auth/utils_test.go
+++ auth/utils_test.go
@@ -3,37 +3,20 @@ package auth
 import (
 	"net/http"
 	"strings"
+	"net/http/httptest"
 
 	"github.com/256dpi/fire"
 	"golang.org/x/crypto/bcrypt"
-	"gopkg.in/mgo.v2/bson"
-	"net/http/httptest"
 )
-
-type Post struct {
-	fire.Base `json:"-" bson:",inline" fire:"posts"`
-	Title     string       `json:"title" valid:"required" bson:"title"`
-	TextBody  string       `json:"text-body" valid:"-" bson:"text_body"`
-	Comments  fire.HasMany `json:"-" valid:"-" bson:"-" fire:"comments:comments:post"`
-}
-
-type Comment struct {
-	fire.Base `json:"-" bson:",inline" fire:"comments"`
-	Message   string         `json:"message" valid:"required"`
-	Parent    *bson.ObjectId `json:"-" valid:"-" fire:"parent:comments"`
-	PostID    bson.ObjectId  `json:"-" valid:"required" bson:"post_id" fire:"post:posts"`
-}
 
 var testStore = fire.MustCreateStore("mongodb://0.0.0.0:27017/test-fire")
 var testSubStore = testStore.Copy()
 
 func cleanSubStore() {
-	testSubStore.DB().C("posts").RemoveAll(nil)
-	testSubStore.DB().C("comments").RemoveAll(nil)
-	testSubStore.DB().C("selections").RemoveAll(nil)
 	testSubStore.DB().C("users").RemoveAll(nil)
 	testSubStore.DB().C("applications").RemoveAll(nil)
 	testSubStore.DB().C("access_tokens").RemoveAll(nil)
+	testSubStore.DB().C("refresh_tokens").RemoveAll(nil)
 }
 
 func newHandler(auth *Manager, force bool) http.Handler {
