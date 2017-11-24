83081de7acdc431fe13e0059d8a412faa0d5f3c8
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-11-24 17:07:06 +0100

    added tools to flame package

--- flame/tools.go
+++ flame/tools.go
@@ -1,6 +1,13 @@
 package flame
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/256dpi/fire/coal"
+	"github.com/pborman/uuid"
+	"gopkg.in/mgo.v2/bson"
+)
 
 // TokenMigrator is a middleware that detects access tokens passed via query
 // parameters and migrates them to a Bearer Token header. Additionally it may
@@ -33,4 +40,108 @@ func TokenMigrator(remove bool) func(http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+// EnsureApplication will ensure that an application with the provided name
+// exists.
+func EnsureApplication(store *coal.Store, name string) error {
+	// copy store
+	s := store.Copy()
+	defer s.Close()
+
+	// count main applications
+	var apps []Application
+	err := s.C(&Application{}).Find(bson.M{
+		coal.F(&Application{}, "Name"): name,
+	}).All(&apps)
+	if err != nil {
+		return err
+	}
+
+	// check existence
+	if len(apps) > 1 {
+		return errors.New("to many applications with that name")
+	} else if len(apps) == 1 {
+		return nil
+	}
+
+	// application is missing
+
+	// create application
+	app := coal.Init(&Application{}).(*Application)
+	app.Key = uuid.New()
+	app.Name = name
+	app.Secret = uuid.New()
+
+	// validate model
+	err = app.Validate()
+	if err != nil {
+		return err
+	}
+
+	// save application
+	err = s.C(app).Insert(app)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetApplicationKey will return the key for the specified application.
+func GetApplicationKey(store *coal.Store, name string) (string, error) {
+	// copy store
+	s := store.Copy()
+	defer s.Close()
+
+	// get application
+	var app Application
+	err := s.C(&app).Find(bson.M{
+		"name": name,
+	}).One(&app)
+	if err != nil {
+		return "", err
+	}
+
+	return app.Key, nil
+}
+
+// EnsureFirstUser ensures the existence of a first user if no other has been
+// created.
+func EnsureFirstUser(store *coal.Store, name, email, password string) error {
+	// TODO: Move to flame.
+
+	// copy store
+	s := store.Copy()
+	defer s.Close()
+
+	// check existence
+	n, err := s.C(&User{}).Count()
+	if err != nil {
+		return err
+	} else if n > 0 {
+		return nil
+	}
+
+	// user is missing
+
+	// create user
+	user := coal.Init(&User{}).(*User)
+	user.Name = name
+	user.Email = email
+	user.Password = password
+
+	// set key and secret
+	err = user.Validate()
+	if err != nil {
+		return err
+	}
+
+	// save user
+	err = s.C(user).Insert(user)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- flame/tools_test.go
+++ flame/tools_test.go
@@ -22,3 +22,33 @@ func TestTokenMigrator(t *testing.T) {
 		assert.Equal(t, "OK", r.Body.String())
 	})
 }
+
+func TestEnsureApplicationAndGetApplicationKey(t *testing.T) {
+	tester.Clean()
+
+	err := EnsureApplication(tester.Store, "Foo")
+	assert.NoError(t, err)
+
+	app := tester.FindLast(&Application{}).(*Application)
+	assert.Equal(t, "Foo", app.Name)
+	assert.NotEmpty(t, app.Key)
+	assert.Empty(t, app.Secret)
+	assert.NotEmpty(t, app.SecretHash)
+
+	key, err := GetApplicationKey(tester.Store, "Foo")
+	assert.NoError(t, err)
+	assert.Equal(t, app.Key, key)
+}
+
+func TestEnsureFirstUser(t *testing.T) {
+	tester.Clean()
+
+	err := EnsureFirstUser(tester.Store, "Foo", "foo@bar.com", "bar")
+	assert.NoError(t, err)
+
+	user := tester.FindLast(&User{}).(*User)
+	assert.Equal(t, "Foo", user.Name)
+	assert.Equal(t, "foo@bar.com", user.Email)
+	assert.Empty(t, user.Password)
+	assert.NotEmpty(t, user.PasswordHash)
+}
