200080b6269971c281c87f07ef9aed3702a24398
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-01-11 22:59:06 +0100

    expose models

--- coal/catalog.go
+++ coal/catalog.go
@@ -2,13 +2,13 @@ package coal
 
 // A Catalog provides a mechanism for models to access each others meta data.
 type Catalog struct {
-	models map[string]Model
+	Models map[string]Model
 }
 
 // NewCatalog will create a new catalog.
 func NewCatalog(models ...Model) *Catalog {
 	g := &Catalog{
-		models: make(map[string]Model),
+		Models: make(map[string]Model),
 	}
 
 	g.Add(models...)
@@ -19,11 +19,11 @@ func NewCatalog(models ...Model) *Catalog {
 // Add will add the specified models to the catalog.
 func (c *Catalog) Add(models ...Model) {
 	for _, model := range models {
-		c.models[Init(model).Meta().PluralName] = model
+		c.Models[Init(model).Meta().PluralName] = model
 	}
 }
 
 // Find will return a model with the specified plural name.
 func (c *Catalog) Find(pluralName string) Model {
-	return c.models[pluralName]
+	return c.Models[pluralName]
 }
