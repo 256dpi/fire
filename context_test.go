6b80f7f5d723f6b8f7d48d9e8e8d238788b8d597
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-02-08 14:47:41 +0200

    added missing tests

--- context_test.go
+++ context_test.go
@@ -10,20 +10,27 @@ import (
 
 func TestOperation(t *testing.T) {
 	table := []struct {
-		op Operation
-		r  bool
-		w  bool
+		o Operation
+		r bool
+		w bool
+		a bool
+		s string
 	}{
-		{List, true, false},
-		{Find, true, false},
-		{Create, false, true},
-		{Update, false, true},
-		{Delete, false, true},
+		{List, true, false, false, "List"},
+		{Find, true, false, false, "Find"},
+		{Create, false, true, false, "Create"},
+		{Update, false, true, false, "Update"},
+		{Delete, false, true, false, "Delete"},
+		{CollectionAction, false, false, true, "CollectionAction"},
+		{ResourceAction, false, false, true, "ResourceAction"},
+		{Operation(0), false, false, false, ""},
 	}
 
 	for _, entry := range table {
-		assert.Equal(t, entry.r, entry.op.Read())
-		assert.Equal(t, entry.w, entry.op.Write())
+		assert.Equal(t, entry.r, entry.o.Read())
+		assert.Equal(t, entry.w, entry.o.Write())
+		assert.Equal(t, entry.a, entry.o.Action())
+		assert.Equal(t, entry.s, entry.o.String())
 	}
 }
 
