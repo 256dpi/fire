cd1a7bee8f6697d3c39f24e4bc88e3da9e70094a
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-03-17 20:19:34 +0100

    added tags

--- axe/job.go
+++ axe/job.go
@@ -25,38 +25,38 @@ type Job struct {
 	coal.Base `json:"-" bson:",inline" coal:"jobs"`
 
 	// The name of the job.
-	Name string
+	Name string `json:"name" bson:"name"`
 
 	// The data that has been supplied on creation.
-	Data bson.Raw
+	Data bson.Raw `json:"data" bson:"data"`
 
 	// The current status of the job.
-	Status Status
+	Status Status `json:"status" bson:"status"`
 
 	// The time when the job was created.
-	Created time.Time
+	Created time.Time `json:"created-at" bson:"created_at"`
 
 	// The time until the job is delayed for execution.
-	Delayed *time.Time
+	Delayed *time.Time `json:"delayed-at" bson:"delayed_at"`
 
 	// The time when the job was the last time dequeued.
-	Started *time.Time
+	Started *time.Time `json:"started-at" bson:"started_at"`
 
 	// The time when the job was ended (completed, failed or cancelled).
-	Ended *time.Time
+	Ended *time.Time `json:"ended-at" bson:"ended_at"`
 
 	// Attempts can be used to determine if a job should be cancelled after too
 	// many attempts.
-	Attempts int
+	Attempts int `json:"attempts" bson:"attempts"`
 
 	// The supplied result submitted during completion.
-	Result bson.M
+	Result bson.M `json:"result" bson:"result"`
 
 	// The error from the last failed attempt.
-	Error string
+	Error string `json:"error" bson:"error"`
 
 	// The reason that has been submitted when the job was cancelled.
-	Reason string
+	Reason string `json:"reason" bson:"reason"`
 }
 
 // AddJobIndexes will add user indexes to the specified indexer. If removeAfter
