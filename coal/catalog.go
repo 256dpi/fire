4ae407691ce2b5341b7a0b9d001e777be1e0dfa6
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-03-17 20:02:42 +0100

    merged files

--- coal/catalog.go
+++ coal/catalog.go
@@ -1,6 +1,10 @@
 package coal
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"sort"
+)
 
 // A Catalog provides a mechanism for models to access each others meta data.
 type Catalog struct {
@@ -52,4 +56,141 @@ func (c *Catalog) All() []Model {
 	}
 
 	return models
+}
+
+// Visualize emits a string in dot format which when rendered with graphviz
+// visualizes the models and their relationships.
+func (c *Catalog) Visualize(title string) string {
+	// prepare buffer
+	var out bytes.Buffer
+
+	// start graph
+	out.WriteString("digraph G {\n")
+	out.WriteString("  rankdir=\"LR\";\n")
+	out.WriteString("  ranksep=\"0.5\";\n")
+	out.WriteString("  nodesep=\"0.4\";\n")
+	out.WriteString("  pad=\"0.4,0.4\";\n")
+	out.WriteString("  margin=\"0,0\";\n")
+	out.WriteString("  concentrate=\"true\";\n")
+	out.WriteString("  labelloc=\"t\";\n")
+	out.WriteString("  fontsize=\"13\";\n")
+	out.WriteString("  fontname=\"Arial BoldMT\";\n")
+	out.WriteString("  splines=\"spline\";\n")
+	out.WriteString("  label=\"" + title + "\";\n")
+
+	// get a sorted list of model names and lookup table
+	var names []string
+	lookup := make(map[string]string)
+	for name, model := range c.models {
+		names = append(names, name)
+		lookup[name] = model.Meta().Name
+	}
+	sort.Strings(names)
+
+	// add model nodes
+	for _, name := range names {
+		// get model
+		model := c.models[name]
+
+		// write begin of node
+		out.WriteString(fmt.Sprintf(`  "%s" [ label=`, lookup[name]))
+
+		// write head table
+		out.WriteString(fmt.Sprintf(`<<table border="0" align="center" cellspacing="0.5" cellpadding="0" width="134"><tr><td align="center" valign="bottom" width="130"><font face="Arial BoldMT" point-size="11">%s</font></td></tr></table>|`, lookup[name]))
+
+		// write begin of tail table
+		out.WriteString(fmt.Sprintf(`<table border="0" align="left" cellspacing="2" cellpadding="0" width="134">`))
+
+		// write attributes
+		for _, field := range model.Meta().OrderedFields {
+			out.WriteString(fmt.Sprintf(`<tr><td align="left" width="130" port="%s">%s<font face="Arial ItalicMT" color="grey60"> %s</font></td></tr>`, field.Name, field.Name, field.Type.String()))
+		}
+
+		// write end of tail table
+		out.WriteString(fmt.Sprintf(`</table>>`))
+
+		// write end of node
+		out.WriteString(`, shape=Mrecord, fontsize=10, fontname="ArialMT", margin="0.07,0.05", penwidth="1.0" ];` + "\n")
+	}
+
+	// define temporary struct
+	type rel struct {
+		from, to   string
+		srcMany    bool
+		dstMany    bool
+		hasInverse bool
+	}
+
+	// prepare list
+	list := make(map[string]*rel)
+	var relNames []string
+
+	// prepare relationships
+	for _, name := range names {
+		// get model
+		model := c.models[name]
+
+		// add all direct relationships
+		for _, field := range model.Meta().OrderedFields {
+			if field.RelName != "" && (field.ToOne || field.ToMany) {
+				list[name+"-"+field.RelName] = &rel{
+					from:    name,
+					to:      field.RelType,
+					srcMany: field.ToMany,
+				}
+
+				relNames = append(relNames, name+"-"+field.RelName)
+			}
+		}
+	}
+
+	// update relationships
+	for _, name := range names {
+		// get model
+		model := c.models[name]
+
+		// add all indirect relationships
+		for _, field := range model.Meta().OrderedFields {
+			if field.RelName != "" && (field.HasOne || field.HasMany) {
+				r := list[field.RelType+"-"+field.RelInverse]
+				r.dstMany = field.HasMany
+				r.hasInverse = true
+			}
+		}
+	}
+
+	// sort relationship names
+	sort.Strings(relNames)
+
+	// add relationships
+	for _, name := range relNames {
+		// get relationship
+		r := list[name]
+
+		// get style
+		style := "solid"
+		if !r.hasInverse {
+			style = "dotted"
+		}
+
+		// get arrow head
+		arrowHead := "none"
+		if r.dstMany {
+			arrowHead = "normal"
+		}
+
+		// get arrow tail
+		arrowTail := "none"
+		if r.srcMany {
+			arrowTail = "normal"
+		}
+
+		// write edge
+		out.WriteString(fmt.Sprintf(`  "%s"->"%s"[ fontname="ArialMT", fontsize=7, dir=both, arrowsize="0.9", penwidth="0.9", labelangle=32, labeldistance="1.8", style=%s, arrowhead=%s, arrowtail=%s ];`, lookup[r.from], lookup[r.to], style, arrowHead, arrowTail) + "\n")
+	}
+
+	// end graph
+	out.WriteString("}\n")
+
+	return out.String()
 }
--- coal/catalog_test.go
+++ coal/catalog_test.go
@@ -21,3 +21,29 @@ func TestCatalog(t *testing.T) {
 
 	assert.Equal(t, []Model{m}, c.All())
 }
+
+func TestCatalogVisualize(t *testing.T) {
+	catalog := NewCatalog(&postModel{}, &commentModel{}, &selectionModel{}, &noteModel{})
+	assert.Equal(t, `digraph G {
+  rankdir="LR";
+  ranksep="0.5";
+  nodesep="0.4";
+  pad="0.4,0.4";
+  margin="0,0";
+  concentrate="true";
+  labelloc="t";
+  fontsize="13";
+  fontname="Arial BoldMT";
+  splines="spline";
+  label="Test";
+  "coal.commentModel" [ label=<<table border="0" align="center" cellspacing="0.5" cellpadding="0" width="134"><tr><td align="center" valign="bottom" width="130"><font face="Arial BoldMT" point-size="11">coal.commentModel</font></td></tr></table>|<table border="0" align="left" cellspacing="2" cellpadding="0" width="134"><tr><td align="left" width="130" port="Message">Message<font face="Arial ItalicMT" color="grey60"> string</font></td></tr><tr><td align="left" width="130" port="Parent">Parent<font face="Arial ItalicMT" color="grey60"> *bson.ObjectId</font></td></tr><tr><td align="left" width="130" port="Post">Post<font face="Arial ItalicMT" color="grey60"> bson.ObjectId</font></td></tr></table>>, shape=Mrecord, fontsize=10, fontname="ArialMT", margin="0.07,0.05", penwidth="1.0" ];
+  "coal.noteModel" [ label=<<table border="0" align="center" cellspacing="0.5" cellpadding="0" width="134"><tr><td align="center" valign="bottom" width="130"><font face="Arial BoldMT" point-size="11">coal.noteModel</font></td></tr></table>|<table border="0" align="left" cellspacing="2" cellpadding="0" width="134"><tr><td align="left" width="130" port="Title">Title<font face="Arial ItalicMT" color="grey60"> string</font></td></tr><tr><td align="left" width="130" port="CreatedAt">CreatedAt<font face="Arial ItalicMT" color="grey60"> time.Time</font></td></tr><tr><td align="left" width="130" port="UpdatedAt">UpdatedAt<font face="Arial ItalicMT" color="grey60"> time.Time</font></td></tr><tr><td align="left" width="130" port="Post">Post<font face="Arial ItalicMT" color="grey60"> bson.ObjectId</font></td></tr></table>>, shape=Mrecord, fontsize=10, fontname="ArialMT", margin="0.07,0.05", penwidth="1.0" ];
+  "coal.postModel" [ label=<<table border="0" align="center" cellspacing="0.5" cellpadding="0" width="134"><tr><td align="center" valign="bottom" width="130"><font face="Arial BoldMT" point-size="11">coal.postModel</font></td></tr></table>|<table border="0" align="left" cellspacing="2" cellpadding="0" width="134"><tr><td align="left" width="130" port="Title">Title<font face="Arial ItalicMT" color="grey60"> string</font></td></tr><tr><td align="left" width="130" port="Published">Published<font face="Arial ItalicMT" color="grey60"> bool</font></td></tr><tr><td align="left" width="130" port="TextBody">TextBody<font face="Arial ItalicMT" color="grey60"> string</font></td></tr><tr><td align="left" width="130" port="Comments">Comments<font face="Arial ItalicMT" color="grey60"> coal.HasMany</font></td></tr><tr><td align="left" width="130" port="Selections">Selections<font face="Arial ItalicMT" color="grey60"> coal.HasMany</font></td></tr><tr><td align="left" width="130" port="Note">Note<font face="Arial ItalicMT" color="grey60"> coal.HasOne</font></td></tr></table>>, shape=Mrecord, fontsize=10, fontname="ArialMT", margin="0.07,0.05", penwidth="1.0" ];
+  "coal.selectionModel" [ label=<<table border="0" align="center" cellspacing="0.5" cellpadding="0" width="134"><tr><td align="center" valign="bottom" width="130"><font face="Arial BoldMT" point-size="11">coal.selectionModel</font></td></tr></table>|<table border="0" align="left" cellspacing="2" cellpadding="0" width="134"><tr><td align="left" width="130" port="Name">Name<font face="Arial ItalicMT" color="grey60"> string</font></td></tr><tr><td align="left" width="130" port="Posts">Posts<font face="Arial ItalicMT" color="grey60"> []bson.ObjectId</font></td></tr></table>>, shape=Mrecord, fontsize=10, fontname="ArialMT", margin="0.07,0.05", penwidth="1.0" ];
+  "coal.commentModel"->"coal.commentModel"[ fontname="ArialMT", fontsize=7, dir=both, arrowsize="0.9", penwidth="0.9", labelangle=32, labeldistance="1.8", style=dotted, arrowhead=none, arrowtail=none ];
+  "coal.commentModel"->"coal.postModel"[ fontname="ArialMT", fontsize=7, dir=both, arrowsize="0.9", penwidth="0.9", labelangle=32, labeldistance="1.8", style=solid, arrowhead=normal, arrowtail=none ];
+  "coal.noteModel"->"coal.postModel"[ fontname="ArialMT", fontsize=7, dir=both, arrowsize="0.9", penwidth="0.9", labelangle=32, labeldistance="1.8", style=solid, arrowhead=none, arrowtail=none ];
+  "coal.selectionModel"->"coal.postModel"[ fontname="ArialMT", fontsize=7, dir=both, arrowsize="0.9", penwidth="0.9", labelangle=32, labeldistance="1.8", style=solid, arrowhead=normal, arrowtail=normal ];
+}
+`, catalog.Visualize("Test"))
+}
