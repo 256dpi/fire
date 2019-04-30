5077250ffabd7058699fdee46ee4ada934b4d8f6
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-04-30 11:37:49 +0300

    test stream resumption

--- coal/stream_test.go
+++ coal/stream_test.go
@@ -69,3 +69,79 @@ func TestStream(t *testing.T) {
 
 	stream.Close()
 }
+
+func TestStreamResumption(t *testing.T) {
+	tester.Clean()
+
+	time.Sleep(100 * time.Millisecond)
+
+	open1 := make(chan struct{})
+	done1 := make(chan struct{})
+
+	var resumeToken []byte
+
+	stream1 := OpenStream(tester.Store, &postModel{}, nil, func(e Event, id bson.ObjectId, m Model, token []byte) {
+		assert.Equal(t, Created, e)
+		assert.NotZero(t, id)
+		assert.NotNil(t, m)
+		assert.NotNil(t, token)
+
+		resumeToken = token
+		close(done1)
+	}, func() {
+		close(open1)
+	}, func(err error) {
+		panic(err)
+	})
+
+	<-open1
+
+	s := tester.Store.Copy()
+	defer s.Close()
+
+	post := Init(&postModel{
+		Title: "foo",
+	}).(*postModel)
+
+	err := s.C(post).Insert(post)
+	assert.NoError(t, err)
+
+	<-done1
+	stream1.Close()
+
+	post.Title = "bar"
+
+	err = s.C(post).UpdateId(post.ID(), post)
+	assert.NoError(t, err)
+
+	err = s.C(post).RemoveId(post.ID())
+	assert.NoError(t, err)
+
+	i := 1
+	done2 := make(chan struct{})
+
+	stream2 := OpenStream(tester.Store, &postModel{}, resumeToken, func(e Event, id bson.ObjectId, m Model, token []byte) {
+		switch i {
+		case 1:
+			assert.Equal(t, Updated, e)
+			assert.NotZero(t, id)
+			assert.NotNil(t, m)
+			assert.NotNil(t, token)
+		case 2:
+			assert.Equal(t, Deleted, e)
+			assert.NotZero(t, id)
+			assert.Nil(t, m)
+			assert.NotNil(t, token)
+
+			close(done2)
+		}
+
+		i++
+	}, nil, func(err error) {
+		panic(err)
+	})
+
+	<-done2
+
+	stream2.Close()
+}
