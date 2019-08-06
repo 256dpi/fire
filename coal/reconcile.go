5563691359f791408a29d981db26ac1fe6163e2a
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-08-06 13:58:31 +0300

    added reconcile function

---
+++ coal/reconcile.go
@@ -0,0 +1,81 @@
+package coal
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Reconcile uses a stream to reconcile changes to a collection. It will
+// automatically load existing models once the underlying stream has been opened.
+// After that it will yield all changes to the collection until the returned
+// stream has been closed.
+func Reconcile(store *Store, model Model, created, updated func(Model), deleted func(primitive.ObjectID), report func(error)) *Stream {
+	// prepare load
+	load := func() error {
+		// get cursor
+		cursor, err := store.C(model).Find(nil, bson.M{})
+		if err != nil {
+			return err
+		}
+
+		// prepare model
+		model := model.Meta().Make()
+
+		// iterate over all documents
+		for cursor.Next(nil) {
+			// decode model
+			err = cursor.Decode(model)
+			if err != nil {
+				return err
+			}
+
+			// call callback if available
+			if created != nil {
+				created(model)
+			}
+		}
+
+		// close cursor
+		err = cursor.Close(nil)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	// open stream
+	stream := OpenStream(store, model, nil, func(event Event, id primitive.ObjectID, model Model, bytes []byte) error {
+		// handle events
+		switch event {
+		case Opened:
+			return load()
+		case Created:
+			// call callback if available
+			if created != nil {
+				created(model)
+			}
+		case Updated:
+			// call callback if available
+			if updated != nil {
+				updated(model)
+			}
+		case Deleted:
+			// call callback if available
+			if deleted != nil {
+				deleted(id)
+			}
+		}
+
+		return nil
+	}, func(err error) bool {
+		// call callback if available
+		if report != nil {
+			report(err)
+		}
+
+		return true
+	})
+
+	return stream
+}
---
+++ coal/reconcile_test.go
@@ -0,0 +1,48 @@
+package coal
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestReconcile(t *testing.T) {
+	tester.Clean()
+
+	time.Sleep(100 * time.Millisecond)
+
+	post := Init(&postModel{
+		Title: "foo",
+	}).(*postModel)
+
+	tester.Save(post)
+
+	open := make(chan struct{})
+	done := make(chan struct{})
+
+	stream := Reconcile(tester.Store, &postModel{}, func(model Model) {
+		assert.Equal(t, post.ID(), model.ID())
+		assert.Equal(t, "foo", model.(*postModel).Title)
+		close(open)
+	}, func(model Model) {
+		assert.Equal(t, post.ID(), model.ID())
+		assert.Equal(t, "bar", model.(*postModel).Title)
+	}, func(id primitive.ObjectID) {
+		assert.Equal(t, post.ID(), id)
+		close(done)
+	}, func(err error) {
+		panic(err)
+	})
+
+	<-open
+
+	post.Title = "bar"
+	tester.Update(post)
+	tester.Delete(post)
+
+	<-done
+
+	stream.Close()
+}
