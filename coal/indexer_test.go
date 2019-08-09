c7127580bf42514d57144027748dda24d09a13a3
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-08-09 13:46:53 +0300

    do not add unique index

--- coal/indexer_test.go
+++ coal/indexer_test.go
@@ -24,10 +24,10 @@ func TestIndexerEnsureError(t *testing.T) {
 	tester.Clean()
 
 	indexer := NewIndexer()
-	indexer.Add(&postModel{}, true, 0, "Published")
+	indexer.Add(&postModel{}, false, 0, "Published")
 	assert.NoError(t, indexer.Ensure(tester.Store))
 
 	indexer = NewIndexer()
-	indexer.Add(&postModel{}, false, 0, "Published")
+	indexer.Add(&postModel{}, true, 0, "Published")
 	assert.Error(t, indexer.Ensure(tester.Store))
 }
