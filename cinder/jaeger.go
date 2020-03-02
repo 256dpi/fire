c12f9017dc69fa58438573b0d1aae7e59a25bfbb
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-03-02 21:21:56 +0200

    do not setup jaeger during benchmarks

--- cinder/jaeger.go
+++ cinder/jaeger.go
@@ -1,6 +1,8 @@
 package cinder
 
 import (
+	"flag"
+
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/transport"
@@ -8,6 +10,12 @@ import (
 
 // SetupTesting will configure a local jaeger instance to receive test traces.
 func SetupTesting(name string) func() {
+	// skip if benchmark
+	if isBenchmark() {
+		println("benchmark detected")
+		return func() {}
+	}
+
 	// create transport
 	tr := transport.NewHTTPTransport("http://0.0.0.0:14268/api/traces?format=jaeger.thrift")
 
@@ -24,4 +32,14 @@ func SetupTesting(name string) func() {
 		_ = closer.Close()
 		_ = tr.Close()
 	}
+}
+
+func isBenchmark() bool {
+	// check bench flag
+	f := flag.Lookup("test.bench")
+	if f != nil && f.Value.String() != "" {
+		return true
+	}
+
+	return false
 }
