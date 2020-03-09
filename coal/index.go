ebd423a5d2e6cafb2a488ea66bb840bc2f9d90d8
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-03-10 00:21:35 +0200

    deduplicate index adding

--- coal/index.go
+++ coal/index.go
@@ -12,10 +12,19 @@ import (
 
 // Index is an index registered with a model.
 type Index struct {
+	// The unprefixed index struct fields.
 	Fields []string
-	Keys   bson.D
+
+	// The translated keys of the index.
+	Keys bson.D
+
+	// Whether the index is unique.
 	Unique bool
+
+	// The automatic expiry of documents.
 	Expiry time.Duration
+
+	// The partial filter expression.
 	Filter bson.D
 }
 
@@ -44,25 +53,7 @@ func (i *Index) Compile() mongo.IndexModel {
 // AddIndex will add an index to the models index list. Fields that are prefixed
 // with a dash will result in an descending key.
 func AddIndex(model Model, unique bool, expiry time.Duration, fields ...string) {
-	// get meta
-	meta := GetMeta(model)
-
-	// get translator
-	trans := NewTranslator(model)
-
-	// translate keys
-	keys, err := trans.Sort(fields)
-	if err != nil {
-		panic(err)
-	}
-
-	// add index
-	meta.Indexes = append(meta.Indexes, Index{
-		Fields: cleanFields(fields),
-		Keys:   keys,
-		Unique: unique,
-		Expiry: expiry,
-	})
+	addIndex(model, unique, expiry, fields, nil)
 }
 
 // AddPartialIndex is similar to AddIndex except that it adds an index with a
@@ -73,6 +64,11 @@ func AddPartialIndex(model Model, unique bool, expiry time.Duration, fields []st
 		panic(`coal: empty partial filter expression`)
 	}
 
+	// add index
+	addIndex(model, unique, expiry, fields, filter)
+}
+
+func addIndex(model Model, unique bool, expiry time.Duration, fields []string, filter bson.D) {
 	// get meta
 	meta := GetMeta(model)
 
@@ -86,14 +82,22 @@ func AddPartialIndex(model Model, unique bool, expiry time.Duration, fields []st
 	}
 
 	// translate filter
-	err = trans.value(filter, false)
-	if err != nil {
-		panic(err)
+	if filter != nil {
+		err = trans.value(filter, false)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// clean fields
+	cleanFields := make([]string, 0, len(fields))
+	for _, field := range fields {
+		cleanFields = append(cleanFields, strings.TrimPrefix(field, "-"))
 	}
 
 	// add index
 	meta.Indexes = append(meta.Indexes, Index{
-		Fields: cleanFields(fields),
+		Fields: cleanFields,
 		Keys:   keys,
 		Unique: unique,
 		Expiry: expiry,
@@ -121,13 +125,4 @@ func EnsureIndexes(store *Store, model Model) error {
 	}
 
 	return nil
-}
-
-func cleanFields(fields []string) []string {
-	list := make([]string, 0, len(fields))
-	for _, field := range fields {
-		list = append(list, strings.TrimPrefix(field, "-"))
-	}
-
-	return list
 }
