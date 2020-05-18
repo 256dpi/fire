9a3209a30030ea8d3c67490f2c9441dd9a9f5ea3
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-05-18 23:29:42 +0200

    stick: add validatable interface

--- stick/validation.go
+++ stick/validation.go
@@ -1,5 +1,10 @@
 package stick
 
+// Validatable represents a type that can be validated.
+type Validatable interface {
+	Validate() error
+}
+
 // NoValidation can be embedded in a struct to provide a no-op validation method.
 type NoValidation struct{}
 
