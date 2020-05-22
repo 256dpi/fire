841c2a1d75e741da39dfbd0057d1a4b006ea7b42
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-05-22 16:39:46 +0200

    added track test

---
+++ cinder/track_test.go
@@ -0,0 +1,21 @@
+package cinder
+
+import (
+	"context"
+	"testing"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrack(t *testing.T) {
+	ctx, span := Track(nil, "foo")
+	assert.Nil(t, ctx)
+	assert.Nil(t, span)
+
+	_, root := opentracing.StartSpanFromContext(context.Background(), "root")
+
+	ctx, span = Track(root, "track")
+	assert.NotNil(t, ctx)
+	assert.NotNil(t, span)
+}
