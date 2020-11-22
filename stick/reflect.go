ca4f1c9ad82987bdc07fe7beadfbac0134d60b4c
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-11-22 22:56:12 +0100

    stick: added numeric getters

--- stick/reflect.go
+++ stick/reflect.go
@@ -32,3 +32,51 @@ func IsNil(v interface{}) bool {
 
 	return false
 }
+
+// GetInt will attempt to get an int64 from the provided value.
+func GetInt(v interface{}) (int64, bool) {
+	switch v := v.(type) {
+	case int:
+		return int64(v), true
+	case int8:
+		return int64(v), true
+	case int16:
+		return int64(v), true
+	case int32:
+		return int64(v), true
+	case int64:
+		return v, true
+	default:
+		return 0, false
+	}
+}
+
+// GetUint will attempt to get an uint64 from the provided value.
+func GetUint(v interface{}) (uint64, bool) {
+	switch v := v.(type) {
+	case uint:
+		return uint64(v), true
+	case uint8:
+		return uint64(v), true
+	case uint16:
+		return uint64(v), true
+	case uint32:
+		return uint64(v), true
+	case uint64:
+		return v, true
+	default:
+		return 0, false
+	}
+}
+
+// GetFloat will attempt to get a float from the provided value.
+func GetFloat(v interface{}) (float64, bool) {
+	switch v := v.(type) {
+	case float32:
+		return float64(v), true
+	case float64:
+		return v, true
+	default:
+		return 0, false
+	}
+}
