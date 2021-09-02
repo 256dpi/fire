36f1226c2d6d98cae5ec31a5a809d4c9ad7a67b2
Joël Gähwiler <joel.gaehwiler@gmail.com>
2021-09-03 00:30:41 +0200

    added access matrices

--- ash/access.go
+++ ash/access.go
@@ -16,6 +16,18 @@ const (
 	Full  = Read | Write
 )
 
+var accessTags = map[rune]Access{
+	' ': None,
+	'L': List,
+	'F': Find,
+	'C': Create,
+	'U': Update,
+	'D': Delete,
+	'R': Read,
+	'W': Write,
+	'*': Full,
+}
+
 // AccessTable defines a string based access table.
 type AccessTable map[string]Access
 
@@ -30,4 +42,42 @@ func (t AccessTable) Collect(match Access) []string {
 	}
 
 	return list
+}
+
+// AccessMatrix defines a string based access matrix.
+type AccessMatrix map[string][]string
+
+// Compile return an access table for the provided column.
+func (m AccessMatrix) Compile(column int) AccessTable {
+	// prepare table
+	table := make(AccessTable, len(m))
+
+	// fill table
+	for key, columns := range m {
+		for _, char := range columns[column] {
+			table[key] |= accessTags[char]
+		}
+	}
+
+	return table
+}
+
+// NamedAccessMatrix defines a named string based access matrix.
+type NamedAccessMatrix struct {
+	Columns []string
+	Matrix  AccessMatrix
+}
+
+// Compile return an access table for the provided column.
+func (m NamedAccessMatrix) Compile(column string) AccessTable {
+	// get index
+	index := -1
+	for i, key := range m.Columns {
+		if key == column {
+			index = i
+			break
+		}
+	}
+
+	return m.Matrix.Compile(index)
 }
---
+++ ash/access_test.go
@@ -0,0 +1,54 @@
+package ash
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccessTable(t *testing.T) {
+	table := AccessTable{
+		"Foo": Find | Create | Delete,
+		"Bar": Read,
+	}
+
+	assert.Equal(t, []string{"Foo", "Bar"}, table.Collect(Read))
+	assert.Equal(t, []string{"Foo"}, table.Collect(Write))
+}
+
+func TestAccessMatrix(t *testing.T) {
+	m := AccessMatrix{
+		"Foo": {"FCD", "*"},
+		"Bar": {"R  ", "W"},
+	}
+
+	assert.Equal(t, AccessTable{
+		"Foo": Find | Create | Delete,
+		"Bar": Read,
+	}, m.Compile(0))
+
+	assert.Equal(t, AccessTable{
+		"Foo": Full,
+		"Bar": Write,
+	}, m.Compile(1))
+}
+
+func TestNamedAccessMatrix(t *testing.T) {
+	m := NamedAccessMatrix{
+		Columns: []string{"foo", "bar"},
+		Matrix: AccessMatrix{
+			"Foo": {"FCD", "*"},
+			"Bar": {"R  ", "W"},
+		},
+	}
+
+	assert.Equal(t, AccessTable{
+		"Foo": Find | Create | Delete,
+		"Bar": Read,
+	}, m.Compile("foo"))
+
+	assert.Equal(t, AccessTable{
+		"Foo": Full,
+		"Bar": Write,
+	}, m.Compile("bar"))
+}
