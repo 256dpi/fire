a79a7865707dc2b36893106aabdc1698f045063d
Joël Gähwiler <joel.gaehwiler@gmail.com>
2021-09-03 00:18:17 +0200

    extracted access

--- ash/policy.go
+++ ash/policy.go
@@ -11,38 +11,6 @@ import (
 	"github.com/256dpi/fire/stick"
 )
 
-// Access defines access levels.
-type Access int
-
-// The available access levels.
-const (
-	None Access = 0
-	List Access = 1 << iota
-	Find
-	Create
-	Update
-	Delete
-	Read  = List | Find
-	Write = Create | Update | Delete
-	Full  = Read | Write
-)
-
-// AccessTable defines a string based access table.
-type AccessTable map[string]Access
-
-// Collect will return all strings with a matching access level.
-func (t AccessTable) Collect(match Access) []string {
-	// collect matches
-	list := make([]string, 0, len(t))
-	for item, access := range t {
-		if access&match != 0 {
-			list = append(list, item)
-		}
-	}
-
-	return list
-}
-
 // PolicyDataKey is the key used to store policies.
 const PolicyDataKey = "ash:policy"
 
