6d6a508fedab86c25a62560ddcb21c56e7edcb2f
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-06-06 21:22:37 +0200

    stick: added registry

---
+++ stick/registry.go
@@ -0,0 +1,96 @@
+package stick
+
+// Registrable is value that can be registered with a Registry.
+type Registrable interface {
+	Validate() error
+}
+
+// Registry is a multi-key index of typed values.
+type Registry[T Registrable] struct {
+	indexer []func(T) string
+	indexes []map[string]T
+	list    []T
+}
+
+// NewRegistry will create and return a new registry using the specified index
+// functions that must return unique keys.
+func NewRegistry[T Registrable](indexer ...func(T) string) *Registry[T] {
+	// created indexes
+	indexes := make([]map[string]T, 0, len(indexer))
+	for range indexer {
+		indexes = append(indexes, map[string]T{})
+	}
+
+	return &Registry[T]{
+		indexer: indexer,
+		indexes: indexes,
+	}
+}
+
+// Add will add the specified values to the registry.
+func (r *Registry[T]) Add(values ...T) {
+	for _, value := range values {
+		// validate value
+		err := value.Validate()
+		if err != nil {
+			panic("stick: invalid value: " + err.Error())
+		}
+
+		// index value
+		for i, indexer := range r.indexer {
+			// get key
+			key := indexer(value)
+			if key == "" {
+				panic("stick: missing key")
+			}
+
+			// check index
+			_, ok := r.indexes[i][key]
+			if ok {
+				panic("stick: value already added")
+			}
+
+			// add to index
+			r.indexes[i][key] = value
+		}
+
+		// add to list
+		r.list = append(r.list, value)
+	}
+}
+
+// Get will attempt lookup a value using the specified predicate.
+func (r *Registry[T]) Get(predicate T) (T, bool) {
+	// check indexes
+	for i, index := range r.indexes {
+		value, ok := index[r.indexer[i](predicate)]
+		if ok {
+			return value, true
+		}
+	}
+
+	// prepare zero value
+	var value T
+
+	return value, false
+}
+
+// MustGet will call Get and panic if not value has been found.
+func (r *Registry[T]) MustGet(predicate T) T {
+	// get value
+	value, ok := r.Get(predicate)
+	if !ok {
+		panic("stick: missing value")
+	}
+
+	return value
+}
+
+// All will return a list of all added values.
+func (r *Registry[T]) All() []T {
+	// copy list
+	list := make([]T, len(r.list))
+	copy(list, r.list)
+
+	return list
+}
---
+++ stick/registry_test.go
@@ -0,0 +1,72 @@
+package stick
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type registryItem struct {
+	Key1 string
+	Key2 string
+}
+
+func (i *registryItem) Validate() error {
+	return Validate(i, func(v *Validator) {
+		v.Value("Key1", false, IsNotZero)
+	})
+}
+
+func TestRegistry(t *testing.T) {
+	registry := NewRegistry[*registryItem](
+		func(item *registryItem) string {
+			return item.Key1
+		},
+		func(item *registryItem) string {
+			return item.Key2
+		},
+	)
+
+	v, ok := registry.Get(&registryItem{})
+	assert.False(t, ok)
+	assert.Nil(t, v)
+
+	assert.PanicsWithValue(t, "stick: missing value", func() {
+		registry.MustGet(&registryItem{})
+	})
+
+	item := &registryItem{
+		Key1: "foo",
+		Key2: "bar",
+	}
+
+	registry.Add(item)
+
+	assert.PanicsWithValue(t, "stick: value already added", func() {
+		registry.Add(item)
+	})
+
+	assert.PanicsWithValue(t, "stick: invalid value: Key1: zero", func() {
+		registry.Add(&registryItem{})
+	})
+
+	assert.PanicsWithValue(t, "stick: missing key", func() {
+		registry.Add(&registryItem{
+			Key1: "baz",
+		})
+	})
+
+	v, ok = registry.Get(&registryItem{
+		Key1: "foo",
+	})
+	assert.True(t, ok)
+	assert.Equal(t, item, v)
+
+	assert.NotPanics(t, func() {
+		registry.MustGet(&registryItem{
+			Key2: "bar",
+		})
+	})
+
+	assert.Equal(t, []*registryItem{item}, registry.All())
+}
